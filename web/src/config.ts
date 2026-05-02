fefdcb5d1b5aa1880850f4b4b7dc09f72a2020bd
imohuan <2652293750@qq.com>
2026-05-03 03:54:07 +0800

    修复链接异常

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,13 +1,12 @@
 export const API_BASE =
   location.port && location.port !== "8080"
     ? `${location.protocol}//${location.hostname}:8080/api`
-    : "/api";
+    : `${location.origin}/api`;
 
 export const CHAT_API_URL = API_BASE.startsWith("/")
   ? `${location.origin}${API_BASE}/chat`
   : `${API_BASE}/chat`;
 
-  
 const _proxyBaseUrl = `${API_BASE}/upload/proxy?url={{url}}&retries=5&retryDelay=500&timeout=15000&maxRetries=5&maxRetryDelay=2000&minTimeout=2000&maxTimeout=20000&type=resource`
 const _proxyOrigin = new URL(_proxyBaseUrl).protocol + "//" + new URL(_proxyBaseUrl).hostname
 
