7f2db56dc7f917584b346234c6e3c436bbf3794e
imohuan <2652293750@qq.com>
2026-05-04 14:54:46 +0800

    删除 yunwu平台， 改为 NewAPI

--- app/src/index.ts
+++ app/src/index.ts
@@ -14,7 +14,7 @@ import { createElysiaApp } from '@adapters/http-elysia';
 import { initPersistence, shutdownPersistence } from '@adapters/persistence';
 import { taskDispatcher } from '@adapters/http-elysia/routes/task.route';
 import { registry } from '@core/application/registry/registry-center';
-import { registerYunwuPlatform, registerNewApiPlatform } from '@/platforms';
+import { registerNewApiPlatform } from '@/platforms';
 import { getConfig, validateConfig } from './config';
 import { Logger, getLogFilePath } from './utils/logger';
 import { existsSync, unlinkSync, mkdirSync, accessSync, constants } from 'fs';
@@ -94,7 +94,6 @@ async function bootstrap() {
 
     // 1. 注册所有平台
     logger.info('📦 注册平台...');
-    registerYunwuPlatform(registry);
     registerNewApiPlatform(registry)
 
     // 未来可以继续注册其他平台
--- app/src/platforms/index.ts
+++ app/src/platforms/index.ts
@@ -2,5 +2,4 @@
  * 平台注册入口
  */
 
-export { registerYunwuPlatform } from '@platforms/yunwu';
 export { registerNewApiPlatform } from "@platforms/newapi"
